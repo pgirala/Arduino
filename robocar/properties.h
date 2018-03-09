74786576d38dc120eecfc31d041aa2c3e8f0ff34
Pablo <pgirala@users.noreply.github.com>
2018-03-09 22:13:39 +0100

    Ajustes de los motores

--- robocar/properties.h
+++ robocar/properties.h
@@ -1,7 +1,7 @@
 #ifndef properties_h
 #define properties_h
  
-#define TEST // TEST activa el modo de pruebas software; para trazas de ejecución poner LOG
+#define LOG // TEST activa el modo de pruebas software; para trazas de ejecución poner LOG
 
 // control remoto
 
@@ -20,13 +20,13 @@
 
 #define NUMERO_MOTORES 4
 #define TIEMPO_PROTECCION_REVERSION 100 // milisegundos
-#define VELOCIDAD_MAXIMA_MOTOR 250
+#define VELOCIDAD_MAXIMA_MOTOR 200
 
 // equilibrado de la velocidad de los motores
 #define AJUSTE_MOTOR_TRASERO_IZQUIERDO 60
-#define AJUSTE_MOTOR_TRASERO_DERECHO 70
-#define AJUSTE_MOTOR_DELANTERO_DERECHO 75
-#define AJUSTE_MOTOR_DELANTERO_IZQUIERDO 70
+#define AJUSTE_MOTOR_TRASERO_DERECHO 75
+#define AJUSTE_MOTOR_DELANTERO_DERECHO 60
+#define AJUSTE_MOTOR_DELANTERO_IZQUIERDO 75
 
 // movimiento
 
